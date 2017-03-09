dff8977c9df08b401cdb473f85391c64ebfde811
Ivan <ivan.baena04@gmail.com>
2017-03-09 09:12:54 -0500

    am push

--- imports/api/lists/methods.js
+++ imports/api/lists/methods.js
@@ -3,11 +3,12 @@ import { HTTP } from 'meteor/http';
 
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=';
 let appId = '&appid=199cc1597806786a2fc3b9125afcb63d';
+let unit = '&units=imperial'
 
 Meteor.methods({
   callWeather: function(query){
     let place = query.query;
     this.unblock();
-    return HTTP.get(url+place+appId, {});
+    return HTTP.get(url+place+unit+appId, {});
   },
 });
--- imports/ui/components/map/map.jsx
+++ imports/ui/components/map/map.jsx
@@ -11,18 +11,18 @@ export default class Map extends Component {
   callWeather() {
     $('#search-form').on('submit', (e) => {
       e.preventDefault();
-      let query = 'bayonne';
       let place = this.refs.search.value;
         Meteor.call("callWeather",{query:place},(error, res) => {
           Session.set('currentWeather', res.data);
             let local = Session.get('currentWeather');
-
+            let iconUrl = "http://openweathermap.org/img/w/" + local.weather[0].icon + ".png";
           if(local) {
-            $('#name').html(local.name);
-            $('#description').html(local.weather[0].description);
-            $('#temp').html(local.main.temp);
+            $('#name-box').html(local.name);
+            $('#description-box').html(local.weather[0].description);
+            $('#icon-box').html("<img src='" + iconUrl  + "'>")
+            $('#temp-box').html(local.main.temp +' degrees');
           }else {
-            $('.weather-data').html('error: ', error);
+            $('.weather-data').html(error);
           }
         });
     })
@@ -35,19 +35,30 @@ export default class Map extends Component {
   render() {
     return (
       <div id="map" className="container">
-        <form id="search-form" className="col-sm-12 col-md-6 col-md-offset-3">
-          <div className="form-group">
-            <input type="text" ref="search" id="search" className="form-control"/>
-          </div>
-          <div className="form-group">
-            <button id="activate" type="submit" className="form-control">WEATHER</button>
-          </div>
-        </form>
+        <div className="sidebar row">
+          <form id="search-form" className="col-sm-12 col-md-12">
+            <div className="form-group col-sm-12 col-md-12">
+              <input type="text" ref="search" id="search" className="form-control"/>
+              <button id="activate" className="btn btn-default" type="submit" className="form-control">WEATHER</button>
+            </div>
+          </form>
+        </div>
 
-          <div className="weather-data" className="row">
-            <div id="name" className="col-sm-12 col-md-4"></div>
-            <div id="description" className="col-sm-12 col-md-4"></div>
-            <div id="temp" className="col-sm-12 col-md-4"></div>
+
+          <div className="weather-data row center-block">
+            <div id="name" className="col-sm-12 col-md-4">
+              <h4>Name</h4>
+              <p id="name-box"></p>
+            </div>
+            <div id="description" className="col-sm-12 col-md-4">
+              <h4>Currently</h4>
+              <div id="icon-box"></div>
+              <p id="description-box"></p>
+            </div>
+            <div id="temp" className="col-sm-12 col-md-4">
+              <h4>Temperature</h4>
+              <p id="temp-box"></p>
+            </div>
           </div>
       </div>
     );
